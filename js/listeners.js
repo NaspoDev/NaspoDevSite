9e944135240c8f6864cbdde570acc3fbbe301e00
Naspo <a.topaltsis@gmail.com>
2023-06-21 23:15:24 -0400

    Abandoned parallax effect. Doesn't look as good as envisioned.

--- js/listeners.js
+++ js/listeners.js
@@ -10,12 +10,3 @@ document.addEventListener("click", (event) => {
 document.addEventListener("scroll", (event) => {
   cursor.remove();
 });
-
-// Parallax scroll effect listener
-// Listens for scroll event
-window.addEventListener("scroll", () => {
-  let box1 = document.getElementById("parallax-box-1");
-  let box2 = document.getElementById("parallax-box-2");
-  box1.style.transform = "translateY(" + window.scrollY * 0.2 + "px)";
-  box2.style.transform = "translateY(" + window.scrollY * -0.2 + "px)";
-});
