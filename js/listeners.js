b463c885e40f5dd0fdf2596a3ef50c12f763a4f3
Naspo <a.topaltsis@gmail.com>
2023-06-22 22:15:55 -0400

    Hover effect for square decors Added the hover effect for square decor elements

--- js/listeners.js
+++ js/listeners.js
@@ -1,5 +1,7 @@
 // Capturing frequently used elements
 const cursor = document.getElementById("cursor");
+const squareDecorOverlay1 = document.getElementById("square-decor-overlay-1");
+const squareDecorOverlay2 = document.getElementById("square-decor-overlay-2");
 
 // Page click anywhere event
 document.addEventListener("click", (event) => {
@@ -9,4 +11,22 @@ document.addEventListener("click", (event) => {
 // Page scroll event
 document.addEventListener("scroll", (event) => {
   cursor.remove();
+});
+
+// square decor mouseover event
+window.addEventListener("mouseover", (event) => {
+  if (event.target === squareDecorOverlay1) {
+    document.getElementById("square-decor-1").style.transform = "scale(0.9)";
+  } else if (event.target === squareDecorOverlay2) {
+    document.getElementById("square-decor-2").style.transform = "scale(0.9)";
+  }
+});
+
+// square decor mouseout event
+window.addEventListener("mouseout", (event) => {
+  if (event.target === squareDecorOverlay1) {
+    document.getElementById("square-decor-1").style.transform = "scale(1)";
+  } else if (event.target === squareDecorOverlay2) {
+    document.getElementById("square-decor-2").style.transform = "scale(1)";
+  }
 });
