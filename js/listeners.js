4f7436873df6a2ead955bdc7abb0d7d959c98ae4
Naspo <a.topaltsis@gmail.com>
2023-07-03 13:55:19 -0400

    Changed the way elements are caputred. They are made constant and not in a initElements function.

--- js/listeners.js
+++ js/listeners.js
@@ -1,26 +1,14 @@
-export function initializeListeners() {
-  // Initialize elements
-  initElements();
+// Capturing frequently used elements
+const cursor = document.getElementById("cursor");
+const squareDecorOverlay1 = document.getElementById("square-decor-overlay-1");
+const squareDecorOverlay2 = document.getElementById("square-decor-overlay-2");
+const squareDecor1 = document.getElementById("square-decor-1");
+const squareDecor2 = document.getElementById("square-decor-2");
 
-  // Initialize listeners
+export function initializeListeners() {
   document.addEventListener("click", handleClick);
   document.addEventListener("scroll", handleScroll);
   squareDecorListeners();
-}
-
-// Capturing frequently used elements
-let cursor;
-let squareDecorOverlay1;
-let squareDecorOverlay2;
-let squareDecor1;
-let squareDecor2;
-
-function initElements() {
-  cursor = document.getElementById("cursor");
-  squareDecorOverlay1 = document.getElementById("square-decor-overlay-1");
-  squareDecorOverlay2 = document.getElementById("square-decor-overlay-2");
-  squareDecor1 = document.getElementById("square-decor-1");
-  squareDecor2 = document.getElementById("square-decor-2");
 }
 
 function handleClick() {
