1da523f27abb47dc982725b74b3bfacf13e8b4cf
Naspo <a.topaltsis@gmail.com>
2023-07-27 15:04:35 -0400

    started work on projects-script.js

---
+++ js/scripts/projects-script.js
@@ -0,0 +1,13 @@
+// Manages animations and display of proper project description for the projects section.
+
+// !! Is this the way I want to do it??
+// Stores class
+let projectItemPairs = [
+  {
+    item: document.querySelector(".big-item"),
+    description: "big-item-description",
+  },
+  { item: "small-item-1", description: "small-item-1-description" },
+  { item: "small-item-2", description: "small-item-2-description" },
+  { item: "long-item", description: "long-item-description" },
+];
