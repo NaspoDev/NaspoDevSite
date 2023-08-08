eb97ce32a5acdf4ff158e6a9cb074e225c7e0544
Naspo <a.topaltsis@gmail.com>
2023-08-08 13:52:02 -0400

    Added a scroll animation to the line-decor

--- js/scripts/services-script.js
+++ js/scripts/services-script.js
@@ -1,1 +1,18 @@
 // Handles other animations for the services section.
+
+let lineDecor = document.getElementById("services-line-decor");
+
+// Animation observer for line decor animation
+const lineDecorObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      lineDecor.classList.add("line-decor-show");
+    } else {
+      lineDecor.classList.remove("line-decor-show");
+    }
+  });
+});
+
+export function run() {
+  lineDecorObserver.observe(lineDecor);
+}
